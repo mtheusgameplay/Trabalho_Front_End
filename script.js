183f744eec29e730692c3a955bb32d97e07c5566
mtheusgameplay <163136497+mtheusgameplay@users.noreply.github.com>
2024-11-06 09:16:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 // Altera o texto do parágrafo inicial
 function alterarTexto() {
-    document.getElementById("text").innerHTML = "Ao clicar, eu mudei!!";
+    document.getElementById("text").innerText = "Ao clicar, eu mudei!!";
 }
 
-// Adicionar um novo parágrafo
+// Adicionar um novo parágrafo ao final do corpo
 function adicionarParagrafo() {
     const novoParagrafo = document.createElement("p");
-    novoParagrafo.innerHTML = "Depois que você clicou, eu apareci🙂  !!";
+    novoParagrafo.innerText = "Depois que você clicou, eu apareci🙂  !!";
     document.body.appendChild(novoParagrafo);
 }
 
 // Remove o último parágrafo, se houver mais de um
 function removerParagrafo() {
-const paragrafos = document.querySelectorAll("p");
-if (paragrafos.length > 1){
-    paragrafos[paragrafos.length - 1].remove();
-} else {
+  const paragrafos = document.querySelectorAll("p");
+  if (paragrafos.length > 1) {
+     paragrafos[paragrafos.length - 1].remove();
+  } else {
     alert("Não há mais parágrafos para remover😢 !!");
-}
-
+  }
 }
 
 // Muda a cor de fundo da página para uma cor aleatória
